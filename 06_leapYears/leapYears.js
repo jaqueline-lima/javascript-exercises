f54dd2d089de4e2fa10403994e150bd5149224d9
Jaqueline <jaquelinedlp78@gmail.com>
2022-03-21 16:31:36 -0300

    Added solution.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,31 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leap,
+        divisable4,
+        divisible100,
+        divisible400;
+    
+    if (year % 4 === 0) {
+        divisable4 = true;
+    }
+    if (year % 100 === 0) {
+        divisible100 = true;
+    }
+    if (year % 400 === 0) {
+        divisible400 = true;
+    }
 
+    if (divisable4) {
+        if (divisible100 && divisible400) {
+            leap = true;
+        } else if (divisible100 && !divisible400) {
+            leap = false;
+        } else if (!divisible100) {
+            leap = true;
+        }
+    } else {
+        leap = false;
+    }
+    return leap;
 };
 
 // Do not edit below this line
