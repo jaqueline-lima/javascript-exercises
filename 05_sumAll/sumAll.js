4f0563c2fc69c1df1b31bada21fc2a9c92ddd10d
Jaqueline <jaquelinedlp78@gmail.com>
2022-03-21 16:02:15 -0300

    Added solution.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let sum = 0;
+    if ((firstNum < 0 || lastNum < 0) || (!Number.isInteger(firstNum) || !Number.isInteger(lastNum))) {
+        return 'ERROR';
+    } else if (firstNum < lastNum) {
+        for (let i = firstNum; i <= lastNum; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = lastNum; i <= firstNum; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 
 };
 
