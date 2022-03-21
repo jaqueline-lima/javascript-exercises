479c88388455d449d903553f25bf8b632116946b
Jaqueline <jaquelinedlp78@gmail.com>
2022-03-21 11:25:52 -0300

    Added solution.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let split = string.split("");
+    let reverse = split.reverse();
+    reverse = reverse.toString();
+    reverse = reverse.replace(/,/g, '');
+    return reverse;
 };
 
 // Do not edit below this line
